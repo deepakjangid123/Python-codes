dd5ba120ca840b4cd72219668a32757202cce91e
Abhishek <deepakjangid123@gmail.com>
2018-05-24 15:37:36 +0530

    :art: Format code a bit

--- es_app/src/es.py
+++ es_app/src/es.py
@@ -11,6 +11,7 @@ MAX = 2147483647
 
 es = Elasticsearch("http://localhost:9200")  # type: Elasticsearch
 
+
 @app.route('/fetch', methods=['POST'])
 def fetch_data_from_es():
     global es
@@ -20,38 +21,38 @@ def fetch_data_from_es():
         params = schema.validate(request.json)
     except Exception as e:
         return Response(json.dumps({'error': str(e)}), mimetype='application/json', status=400)
-    pluck = lambda dict, *args: (params[arg] for arg in args)
+    pluck = lambda dict, *args: (dict[arg] for arg in args)
     db, fields = pluck(params, 'db', 'fields')  # destructuring of a map
     res = es.search(index=db, body={
-                                    "query": {
-                                      "bool": {
-                                        "filter": {
-                                          "bool": {
-                                            "should": [
-                                              {
-                                                "bool": {
-                                                  "must": [
-                                                    {
-                                                      "terms": {
-                                                          str(fields[0]) + '.keyword': ["1st"]
-                                                      }
-                                                    }
-                                                  ]
-                                                }
-                                              }
-                                            ]
-                                          }
+        "query": {
+            "bool": {
+                "filter": {
+                    "bool": {
+                        "should": [
+                            {
+                                "bool": {
+                                    "must": [
+                                        {
+                                            "terms": {
+                                                str(fields[0]) + '.keyword': ["1st"]
+                                            }
                                         }
-                                      }
-                                    },
-                                    "aggs": {
-                                      "c-id": {
-                                        "terms": dict(field=fields[1], size=MAX)
-                                      }
-                                    },
-                                    "from": 0,
-                                    "size": 0
-                                  })
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        "aggs": {
+            "c-id": {
+                "terms": dict(field=fields[1], size=MAX)
+            }
+        },
+        "from": 0,
+        "size": 0
+    })
     return Response(json.dumps(res), mimetype='application/json', status=200)
 
 
